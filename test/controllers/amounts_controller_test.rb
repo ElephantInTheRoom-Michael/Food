2f0209d61939786b35b85835c62ce8e1b4d1a74c
Michael Taylor <241386356+ElephantInTheRoom-Michael@users.noreply.github.com>
2026-02-12 17:31:34 -0800

    Generate scaffold

---
+++ test/controllers/amounts_controller_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class AmountsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @amount = amounts(:one_onion)
+  end
+
+  test "should get index" do
+    get amounts_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_amount_url
+    assert_response :success
+  end
+
+  test "should create amount" do
+    assert_difference("Amount.count") do
+      post amounts_url, params: {
+        amount: {
+          ingredient_id: @amount.ingredient.id,
+          serving: @amount.serving + 1.0,
+        },
+      }
+    end
+
+    assert_redirected_to amount_url(Amount.last)
+  end
+
+  test "should show amount" do
+    get amount_url(@amount)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_amount_url(@amount)
+    assert_response :success
+  end
+
+  test "should update amount" do
+    patch amount_url(@amount), params: { amount: { serving: @amount.serving, serving_variant: @amount.serving_variant, volume: @amount.volume, volume_variant: @amount.volume_variant, weight: @amount.weight } }
+    assert_redirected_to amount_url(@amount)
+  end
+
+  # test "should destroy amount" do
+  #   assert_difference("Amount.count", -1) do
+  #     delete amount_url(@amount)
+  #   end
+  #
+  #   assert_redirected_to amounts_url
+  # end
+end
